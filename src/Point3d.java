ce4771d52bb933f9d5781f734a0baceec4ae90ba
Stevenochka <redlemmon714@gmail.com>
2022-10-20 23:40:28 +0300

    Ну, пока так...

--- src/Point3d.java
+++ src/Point3d.java
@@ -1,2 +1,31 @@
-public class Point3d {
+public class Point3d extends Point2d{
+    private double Zcoord;
+
+    public Point3d(double x, double y, double z){
+        super(x,y);
+        Zcoord = z;
+    }
+
+    public Point3d(){
+        this(0,0,0);
+    }
+
+    public void setZ(double z){
+        Zcoord = z;
+    }
+
+    public double getZ(){
+        return Zcoord;
+    }
+
+    public boolean compare(Point3d point){
+        return (this.getX() == point.getX() && this.getY() == point.getY() && Zcoord == point.getX());
+    }
+
+    public double distanceTo(Point3d point){
+        return Math.sqrt(Math.pow(this.getX() - point.getX(), 2)
+                + Math.pow(this.getY() - point.getY(), 2)
+                + Math.pow(Zcoord - point.getZ(), 2));
+    }
+
 }
--- src/lab2.java
+++ src/lab2.java
@@ -1,2 +1,42 @@
+import java.util.Scanner;
+
 public class lab2 {
+    public static void main(String[] args){
+
+        Point3d[] points = new Point3d[3];
+        double[] coords = new double[3];
+
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 0; i < 3; i++){
+
+            for(int j = 0; j < 3; j++){
+                char coord;
+                if      (j == 0)    coord = 'x';
+                else if (j == 1)    coord = 'y';
+                else                coord = 'z';
+                System.out.print("Введите координату " + coord + " точки " + (i + 1) + ": ");
+                coords[j] = scanner.nextDouble();
+            }
+            points[i] = new Point3d(coords[0], coords[1], coords[2]);
+        }
+        if(points[0].compare(points[1]) || points[0].compare(points[2]) || points[1].compare(points[2])) {
+            System.out.println("Одна из точек совпадает с другой!");
+            return;
+        }
+
+        double area = computeArea(points[0], points[1], points[2]);
+        if(area == 0){
+            System.out.println("Точки лежат на одной прямой!");
+            return;
+        }
+        System.out.println("Площадь треугольника: " + area);
+    }
+
+    public static double computeArea(Point3d point1, Point3d point2, Point3d point3){
+        double a = point1.distanceTo(point2);
+        double b = point1.distanceTo(point3);
+        double c = point2.distanceTo(point3);
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
 }
